6cdcda1bc663505152946ebae8c7d1bfa122ffcc
yktohub <rbgh2858@naver.com>
2023-12-08 12:05:50 +0900

    초기 myWallet 값 1000원으로 수정 주문 시 잔액표시 (%.2f 포맷으로 출력, ex)980.21 W) 수정 잔액 부족시 표시되는 문구도 %.2f 포맷으로 수정

--- nbc_Team9_Kiosk/Order.swift
+++ nbc_Team9_Kiosk/Order.swift
@@ -7,16 +7,15 @@
 
 class Order {
     
-    var myWallet: Double = 20 //초기 잔액 설정 20 w
+    var myWallet: Double = 1000 //초기 잔액 설정 1000 w
     var totalPrice: Double = 0 //장바구니에 담긴 상품들의 총 합 가격
     
     func order(_ price: Double, cartInstance: Cart){
         totalPrice = price
         
         print("아래와 같이 주문 하시겠습니까?")
-        //print("\(cart.products)")
         print("\n[ Total ]")
-        print("\(totalPrice)\n")
+        print("\(totalPrice)W\n")
         print("1. 주문       2. 메뉴판")
         
         if let input = readLine() {
@@ -24,6 +23,8 @@ class Order {
                 switch input {
                 case "1":
                     print("주문을 성공했습니다.")
+                    myWallet -= totalPrice
+                    print("현재 잔액은 \(String(format: "%.2f", myWallet))W 입니다.")
                     cartInstance.removeCart()
                 case "2":
                     print("이전 메뉴로 돌아갑니다.")
@@ -32,7 +33,7 @@ class Order {
                     print("다시 입력해주세요")
                 }
             }else{
-                print("현재 잔액은 \(myWallet)W 으로 \(totalPrice-myWallet)W 이 부족해서 주문할 수 없습니다.")
+                print("현재 잔액은 \(String(format: "%.2f", myWallet))W 으로 \(String(format: "%.2f", totalPrice - myWallet))W 이 부족해서 주문할 수 없습니다.")
             }
         }
     }
